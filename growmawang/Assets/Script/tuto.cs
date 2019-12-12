68670f00e1e94fb9ea84df3780bbf5883a1495c5
leejeongseok <jsuk10@naver.com>
2019-12-12 17:29:49 +0900

    튜토리얼 추가 정보 삽입

--- growmawang/Assets/Script/tuto.cs
+++ growmawang/Assets/Script/tuto.cs
@@ -29,23 +29,38 @@ public class tuto : MonoBehaviour
     {
         if (count == 1)
         {
-            bt1.image.sprite = image[1];
+            bt1.image.sprite = image[count++];
             tx.text = "자동으로 식물의 위치쪽으로 이동합니다.";
-            count =2;
             return;
         }
         if (count == 2)
         {
-            bt1.image.sprite = image[2];
+            bt1.image.sprite = image[count++];
             tx.text = "식물의 앞에 오면 수확 버튼을 눌러 수확합니다.";
-            count = 3;
             return;
         }
         if (count == 3)
         {
-            bt1.image.sprite = image[3];
-            tx.text = "정확하게 클릭하지 않으면 사망합니다." +"\n" + " 다른 식물을 수확시 추가 효과가 있습니다.";
-            count = 4;
+            bt1.image.sprite = image[count++];
+            tx.text = "정확하게 클릭하지 않으면 사망합니다." + "\n" + " 다른 식물을 수확시 추가 효과가 있습니다.";
+            return;
+        }
+        if (count == 4)
+        {
+            bt1.image.sprite = image[count++];
+            tx.text = "특수 식물이 나오기 전 특수한 모양이 나타납니다" + "\n" + "(좌측 몬스터 구간)";
+            return;
+        }
+        if (count == 5)
+        {
+            bt1.image.sprite = image[count++];
+            tx.text = "물을 더 많이 주어서 식물을 수확해야 합니다.";
+            return;
+        }
+        if (count == 6)
+        {
+            bt1.image.sprite = image[count++];
+            tx.text = "식물마다 수확시 특수한 기능이 작용합니다." + "\n" + "(밑의 식물은 시간을 0.25초 늘려줍니다.)";
             return;
         }
         else
@@ -53,7 +68,7 @@ public class tuto : MonoBehaviour
             count = 1;
             bt1.image.sprite = image[0];
             howtoplay.SetActive(false);
-            tx.text = "스마트 폰의 오른쪽을 클릭하여 식물쪽으로 이동합니다.";
+            tx.text = "스마트 폰의 오른쪽을 클릭하여 이동합니다.";
             return;
         }
     }
